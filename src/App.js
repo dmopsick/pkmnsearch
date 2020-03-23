e5f5f84c9fa714067838b2d04c38a30941418f5d
Dan Mopsick <dmop497@gmail.com>
2020-03-23 12:27:13 -0400

    #1 committing the project to git.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,63 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import TextField from '@material-ui/core/TextField';
+import { makeStyles } from '@material-ui/core/styles';
+import MenuItem from '@material-ui/core/MenuItem';
+
+const useStyles = makeStyles({
+  textField: {
+    color: 'white',
+  },
+});
+
+// List of each generation
+// label - what to display in the dropdown
+// value - the pokedex link used to form the URL to serebii
+const generations = [
+  {
+    label: 'Gen 1',
+    value: 'pokedex'
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  },
+  {
+
+  }
+];
 
 function App() {
+  const classes = useStyles();
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+          Open the Serebii page you want, fast.
+
+          <TextField id="pokemonName" className={classes.textField} label="Enter Pokémon Name."/>
+
+          <select>
+            <option>Select the generation.</option>
+          </select>
+
+
+
       </header>
     </div>
   );
