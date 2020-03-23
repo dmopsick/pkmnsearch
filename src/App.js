db23a30281bb45010bc8f99b18839b2b8f550969
Dan Mopsick <dmop497@gmail.com>
2020-03-23 12:47:03 -0400

    Issue #2 adding in the drop down menu component. Have not verified functionality.

--- src/App.js
+++ src/App.js
@@ -6,8 +6,8 @@ import { makeStyles } from '@material-ui/core/styles';
 import MenuItem from '@material-ui/core/MenuItem';
 
 const useStyles = makeStyles({
-  textField: {
-    color: 'white',
+  field: {
+    margin: '25px',
   },
 });
 
@@ -20,41 +20,72 @@ const generations = [
     value: 'pokedex'
   },
   {
-
+    label: 'Gen 2',
+    value: 'pokedex-gs'
   },
   {
-
+    label: 'Gen 3',
+    value: 'pokedex-rs'
   },
   {
-
+    label: 'Gen 4',
+    value: 'pokedex-dp'
   },
   {
-
+    label: 'Gen 5',
+    value: 'pokedex-bw'
   },
   {
-
+    label: 'Gen 6',
+    value: 'pokedex-xy'
   },
   {
-
+    label: 'Gen 7',
+    value: 'pokedex-sm'
   },
   {
-
-  }
+    label: 'Gen 8',
+    value: 'pokedex-swsh'
+  },
 ];
 
 function App() {
   const classes = useStyles();
 
+  let generation, pokemonName;
+
+  const handleChange = event => {
+    generation = event.target.value;
+  };
+
   return (
     <div className="App">
       <header className="App-header">
-          Open the Serebii page you want, fast.
+        Open the Serebii page you want, fast.
 
-          <TextField id="pokemonName" className={classes.textField} label="Enter Pokémon Name."/>
+          <TextField id="pokemonName" value={pokemonName} className={classes.field} label="Enter Pokémon Name." />
 
-          <select>
-            <option>Select the generation.</option>
-          </select>
+        <TextField
+          id="select-generation"
+          select
+          className={classes.field}
+          label="Select the gen"
+          value={generation}
+          onChange={handleChange}
+          helperText="Select which generation you want info for."
+        >
+          {generations.map(gen => (
+            <MenuItem key="gen.value" value={gen.value}>
+              {gen.label}
+            </MenuItem>
+          ))}
+        </TextField>
+
+        <a>
+          
+        </a>
+
+
 
 
 
